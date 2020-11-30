50827bc6f35f2b1a133459d9b0654d2e34e92e05
mueller-ma <mueller-ma@users.noreply.github.com>
2020-11-30 23:46:00 +0100

    Add some debug logging
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- app/src/main/java/com/github/muellerma/coffee/CoffeeTile.kt
+++ app/src/main/java/com/github/muellerma/coffee/CoffeeTile.kt
@@ -2,6 +2,7 @@ package com.github.muellerma.coffee
 
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.service.quicksettings.Tile.STATE_ACTIVE
 import android.service.quicksettings.Tile.STATE_INACTIVE
 import android.service.quicksettings.TileService
@@ -15,19 +16,49 @@ class CoffeeTile : TileService() {
 
     override fun onStartListening() {
         Log.d(TAG, "onStartListening()")
-        super.onStartListening()
         setTileState()
+        super.onStartListening()
     }
 
     override fun onStopListening() {
         Log.d(TAG, "onStopListening()")
-        super.onStopListening()
         setTileState()
+        super.onStopListening()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy()")
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        Log.d(TAG, "onLowMemory()")
+        super.onLowMemory()
+    }
+
+    override fun onTileAdded() {
+        Log.d(TAG, "onTileAdded()")
+        super.onLowMemory()
+    }
+
+    override fun onTileRemoved() {
+        Log.d(TAG, "onTileRemoved()")
+        super.onLowMemory()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        Log.d(TAG, "onTrimMemory()")
+        super.onTrimMemory(level)
+    }
+
+    override fun onRebind(intent: Intent?) {
+        Log.d(TAG, "onRebind()")
+        super.onRebind(intent)
     }
 
     private fun setTileState() {
         val isRunning = (application as CoffeeApplication).isRunning
-        Log.d(TAG, "setTileState: running = $isRunning")
+        Log.d(TAG, "setTileState(): running = $isRunning")
         val tile = qsTile ?: return
         tile.apply {
             state = if (isRunning) STATE_ACTIVE else STATE_INACTIVE

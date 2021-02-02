d8606d1a47d2c748bb5b6d75e2bed32a6eb588ef
mueller-ma <mueller-ma@users.noreply.github.com>
2021-02-02 11:41:03 +0100

    Optimize imports

--- app/src/main/java/com/github/muellerma/coffee/MainActivity.kt
+++ app/src/main/java/com/github/muellerma/coffee/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.github.muellerma.coffee
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle

08e6dab7062b78da3322a8770cb100b57b596350
mueller-ma <mueller-ma@users.noreply.github.com>
2022-12-08 08:42:01 +0100

    Check action in BootReceiver

--- app/src/main/java/com/github/muellerma/coffee/BootReceiver.kt
+++ app/src/main/java/com/github/muellerma/coffee/BootReceiver.kt
@@ -7,6 +7,10 @@ import android.os.Build
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED) {
+            return
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             CoffeeTile.requestTileStateUpdate(context)
         }
